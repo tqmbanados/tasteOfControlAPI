3631e537e636198d34d1dc1fe0348c549408d87c
TOM BAÑADOS <tbanados@uc.cl>
2022-04-03 18:50:33 -0400

    added urls

--- tasteofapi/backend/urls.py
+++ tasteofapi/backend/urls.py
@@ -2,10 +2,11 @@ from django.urls import include, path
 
 from rest_framework import routers
 
-from .views import MainScoreView
+from .views import MainScoreView, InstrumentView
 
 router = routers.DefaultRouter()
 router.register(r'', MainScoreView)
+router.register(r'instrument', InstrumentView)
 
 urlpatterns = [
    path('', include(router.urls)),
