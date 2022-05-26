4d79024ef1f85aa306f9eaa356b839aa2ebfe910
TOM BAÑADOS <tbanados@uc.cl>
2022-05-26 02:30:48 -0400

    fixed actor rendering

---
+++ backend/custom_renderer.py
@@ -0,0 +1,11 @@
+from rest_framework import renderers
+
+
+class PNGRenderer(renderers.BaseRenderer):
+    media_type = 'image/png'
+    format = 'png'
+    charset = None
+    render_style = 'binary'
+
+    def render(self, data, *args, **kwargs):
+        return data
--- backend/views.py
+++ backend/views.py
@@ -7,6 +7,7 @@ from rest_framework import status
 from rest_framework.parsers import MultiPartParser, FormParser
 from .serializers import (FullScoreSerializer, ActorSerializer,
                           InstrumentSerializer, ImageSerializer)
+from .custom_renderer import PNGRenderer
 import os
 
 
@@ -66,28 +67,26 @@ class ImageView(APIView):
     queryset = Instrument.objects.all()
     serializer_class = ImageSerializer
     parser_classes = [MultiPartParser, FormParser]
+    renderer_classes = [PNGRenderer]
 
-    def get(self, request, format=None):
+    def get(self, request, format=None, *args, **kwargs):
         instrument_name = request.query_params.get('instrument')
         request_measure = request.query_params.get('measure')
         query = self.queryset.filter(instrument=instrument_name)
         if len(query) > 0:
             instrument = query.first()
-            serialized = self.serializer_class()
+            data = instrument.image_data
             measure = instrument.measure
             if measure != int(request_measure):
                 print("WARNING: REQUESTED MEASURE DIFFERENT FROM HELD MEASURE\n",
                       f'Requested: {request_measure}. Current: {measure}')
-            if serialized.is_valid():
-                return Response(serialized.data, status=status.HTTP_200_OK)
-            else:
-                return Response(serialized.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response(data, status=status.HTTP_200_OK,
+                            content_type='image/png')
         return Response('hi', status=status.HTTP_204_NO_CONTENT)
 
     def patch(self, request, format=None):
         instrument_model = self.queryset.filter(instrument=request.data.get('instrument')).first()
         serializer = self.serializer_class(instrument_model, data=request.data)
-
         if serializer.is_valid():
             instrument_model.image_data = request.FILES['media']
             serializer.save()
@@ -110,7 +109,7 @@ class ActorView(APIView):
             return Response(data, status=status.HTTP_200_OK)
         else:
             if len(self.queryset.all()) > 0:
-                actor = self.queryset[-1]
+                actor = self.queryset[0]
                 data = {'action': actor.action,
                         'stage': actor.stage}
                 return Response(data, status=status.HTTP_206_PARTIAL_CONTENT)
